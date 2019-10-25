1f3bf31831ea5e06596d72c583fc072448ed03f2
hrt-yokoyama <hiroyoko256@gmail.com>
2019-10-25 20:39:24 +0900

    frame

--- Halloween_v2/app.js
+++ Halloween_v2/app.js
@@ -6,7 +6,7 @@ const cameraView = document.querySelector("#camera--view"),
     cameraSensor = document.querySelector("#camera--sensor"),
     cameraTrigger = document.querySelector("#camera--trigger")
     cameraFrame = document.querySelector("#camera--frame")
-
+    cameraStill = document.querySelector("#camera--still")
 
 
 // Access the device camera and stream to cameraView
@@ -16,13 +16,27 @@ function cameraStart() {
         .then(function(stream) {
         track = stream.getTracks()[0];
         cameraView.srcObject = stream;
+        drawFrame();
     })
     .catch(function(error) {
         console.error("Oops. Something is broken.", error);
     });
 }
 
+// Take a picture when cameraTrigger is tapped
+cameraTrigger.onclick = function() {
+    cameraSensor.width = cameraView.videoWidth;
+    cameraSensor.height = cameraView.videoHeight;
+    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0)
+    cameraOutput.src = cameraSensor.toDataURL("image/webp");
+    cameraOutput.classList.add("taken");
+};
+// Start the video stream when the window loads
+window.addEventListener("load", cameraStart, false);
+
 function drawFrame(){
+    const image = new Image();
+    image.src = cameraFrame.src;
     image.onload = () => {
       const ctx = cameraFrame.getContext("2d");
       //ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
@@ -31,15 +45,3 @@ function drawFrame(){
     };
 }
 
-// Take a picture when cameraTrigger is tapped
-cameraTrigger.onclick = function() {
-    cameraSensor.width = cameraView.videoWidth;
-    cameraSensor.height = cameraView.videoHeight;
-    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
-    cameraOutput.src = cameraSensor.toDataURL("image/webp");
-    cameraOutput.classList.add("taken");
-};
-// Start the video stream when the window loads
-window.addEventListener("load", cameraStart, false);
-
-drawFrame();
